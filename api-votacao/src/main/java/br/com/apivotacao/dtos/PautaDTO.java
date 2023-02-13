f90158d1c204c294489379e936f461e26d0966e8
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-02-13 11:41:17 -0300

    PautaDTO

---
+++ api-votacao/src/main/java/br/com/apivotacao/dtos/PautaDTO.java
@@ -0,0 +1,27 @@
+package br.com.apivotacao.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PautaDTO {
+
+    @NotBlank(message = "Assunto não pode está nulo ou vazio.")
+    private String assunto;
+
+    @NotBlank(message = "Descrição não pode está nulo ou vazio.")
+    private String descricao;
+
+    private Boolean sessaoAtiva;
+
+    private LocalDateTime dataCadastro;
+
+    private LocalDateTime dataEncerramento;
+
+}
