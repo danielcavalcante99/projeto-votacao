f37454fb216146935344176e212fa69573f5b42d
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-02-14 07:06:00 -0300

    Projeto Criado

---
+++ microservico-votacao/src/main/java/br/com/microservicovotacao/MicroservicoVotacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservicovotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoVotacaoApplication.class, args);
+	}
+
+}
